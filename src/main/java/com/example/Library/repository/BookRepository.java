93b8ace6908c4b16cf18ebd53cf86feaecf77b57
bhaskarpattepu <Bhaskar.pattepu123@gmail.com>
2025-03-14 11:59:55 +0530

    space delete

--- src/main/java/com/example/Library/repository/BookRepository.java
+++ src/main/java/com/example/Library/repository/BookRepository.java
@@ -3,9 +3,6 @@ package com.example.Library.repository;
 import com.example.Library.model.Book;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long>{
 }
