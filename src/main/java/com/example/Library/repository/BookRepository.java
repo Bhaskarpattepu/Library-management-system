100cda29bc96ab12ffbc70aaf83ef2dce44e93ad
bhaskarpattepu <Bhaskar.pattepu123@gmail.com>
2025-03-14 11:55:09 +0530

    space

--- src/main/java/com/example/Library/repository/BookRepository.java
+++ src/main/java/com/example/Library/repository/BookRepository.java
@@ -3,6 +3,9 @@ package com.example.Library.repository;
 import com.example.Library.model.Book;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long>{
 }
